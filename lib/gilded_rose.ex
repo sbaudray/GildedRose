a506c6e7c402d47b34307e33f2895777f384c4dd
Sebastien Baudray <baudray.sebastien@gmail.com>
2020-09-17 17:16:07 +0200

    Simplify

--- lib/gilded_rose.ex
+++ lib/gilded_rose.ex
@@ -3,24 +3,15 @@ defmodule GildedRose do
     Enum.map(items, &update_item/1)
   end
 
-  def update_item(%Item{name: "Aged Brie", sell_in: sell_in, quality: quality} = item)
-      when sell_in <= 0 and quality <= 48 do
-    %{item | sell_in: item.sell_in - 1, quality: item.quality + 2}
-  end
+  def update_item(%Item{name: "Aged Brie", quality: quality, sell_in: sell_in} = item) do
+    new_quality =
+      cond do
+        sell_in <= 0 -> quality + 2
+        true -> quality + 1
+      end
+      |> clamp()
 
-  def update_item(%Item{name: "Aged Brie", quality: quality} = item) when quality < 50 do
-    %{item | sell_in: item.sell_in - 1, quality: item.quality + 1}
-  end
-
-  def update_item(%Item{name: "Aged Brie"} = item) do
-    %{item | sell_in: item.sell_in - 1}
-  end
-
-  def update_item(
-        %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: sell_in} = item
-      )
-      when sell_in <= 0 do
-    %{item | sell_in: item.sell_in - 1, quality: 0}
+    %{item | sell_in: item.sell_in - 1, quality: new_quality}
   end
 
   def update_item(
@@ -29,47 +20,39 @@ defmodule GildedRose do
           sell_in: sell_in,
           quality: quality
         } = item
-      )
-      when sell_in <= 5 and quality <= 47 do
-    %{item | sell_in: item.sell_in - 1, quality: item.quality + 3}
-  end
+      ) do
+    new_quality =
+      cond do
+        sell_in <= 0 -> 0
+        sell_in <= 5 -> quality + 3
+        sell_in <= 10 -> quality + 2
+        true -> quality + 1
+      end
+      |> clamp()
 
-  def update_item(
-        %Item{
-          name: "Backstage passes to a TAFKAL80ETC concert",
-          sell_in: sell_in,
-          quality: quality
-        } = item
-      )
-      when sell_in <= 10 and quality <= 48 do
-    %{item | sell_in: item.sell_in - 1, quality: item.quality + 2}
-  end
-
-  def update_item(
-        %Item{name: "Backstage passes to a TAFKAL80ETC concert", quality: quality} = item
-      )
-      when quality <= 49 do
-    %{item | sell_in: item.sell_in - 1, quality: item.quality + 1}
-  end
-
-  def update_item(%Item{name: "Backstage passes to a TAFKAL80ETC concert"} = item) do
-    %{item | sell_in: item.sell_in - 1}
+    %{item | sell_in: item.sell_in - 1, quality: new_quality}
   end
 
   def update_item(%Item{name: "Sulfuras, Hand of Ragnaros"} = item) do
     item
   end
 
-  def update_item(%Item{sell_in: sell_in, quality: quality} = item)
-      when sell_in <= 0 and quality >= 2 do
-    %{item | sell_in: item.sell_in - 1, quality: item.quality - 2}
+  def update_item(%Item{quality: quality, sell_in: sell_in} = item) do
+    new_quality =
+      cond do
+        sell_in <= 0 -> quality - 2
+        true -> quality - 1
+      end
+      |> clamp()
+
+    %{item | sell_in: item.sell_in - 1, quality: new_quality}
   end
 
-  def update_item(%Item{quality: quality} = item) when quality > 0 do
-    %{item | sell_in: item.sell_in - 1, quality: item.quality - 1}
-  end
-
-  def update_item(%Item{} = item) do
-    %{item | sell_in: item.sell_in - 1}
+  defp clamp(quality) do
+    cond do
+      quality < 0 -> 0
+      quality > 50 -> 50
+      true -> quality
+    end
   end
 end
